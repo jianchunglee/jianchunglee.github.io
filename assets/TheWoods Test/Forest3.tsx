2f55b9f73ee1f6bf714807646aa2130019883714
J.C. Lee <jianchunglee@gmail.com>
2018-11-11 23:43:20 +0100

    Resized maps for TheWoods test

---
+++ assets/TheWoods Test/Forest3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Forest3" tilewidth="32" tileheight="32" tilecount="1000" columns="50">
+ <image source="Forest.png" trans="9a9f9c" width="1600" height="640"/>
+</tileset>
